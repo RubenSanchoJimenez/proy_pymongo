73a9da478582c0b4ef377536b2dbf13467cd07fd
RubenSanchoJimenez <ruben030502@gmail.com>
2024-11-05 14:00:05 +0100

    FEAT - maked cards in home and routing

--- src/app/crud/read/read.component.ts
+++ src/app/crud/read/read.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-read',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './read.component.html',
   styleUrl: './read.component.css'
 })
