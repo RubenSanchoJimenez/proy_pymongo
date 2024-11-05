a3a479816657467848dfc191c9b53f5ccba82c14
RubenSanchoJimenez <ruben030502@gmail.com>
2024-11-05 18:07:03 +0100

    FEAT - set documents in JSON

--- src/app/crud/create/create.component.ts
+++ src/app/crud/create/create.component.ts
@@ -12,4 +12,33 @@ import { RouterLink } from '@angular/router';
 })
 export class CreateComponent {
 
+  constructor(){
+
+  }
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const archivo = input.files[0];
+
+      // Verificar que el archivo sea .json
+      if (archivo.type === "application/json") {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          try {
+            const contenido = JSON.parse(e.target?.result as string);
+            console.log("Contenido del archivo JSON:", contenido);
+          } catch (error) {
+            console.error("El archivo no es un JSON válido.");
+          }
+        };
+        reader.readAsText(archivo);
+      } else {
+        console.error("Por favor selecciona un archivo .json");
+      }
+    }
+  }
+
+  
+
 }
