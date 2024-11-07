d3f52b2bf404c25973351e22ce5fea193bb7b41e
RubenSanchoJimenez <ruben030502@gmail.com>
2024-11-07 20:40:18 +0100

    UPDATE - style + full program OK

---
+++ src/app/about-that/about-that.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-that',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-that.component.html',
+  styleUrl: './about-that.component.css'
+})
+export class AboutThatComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {HomeComponent} from "./home/home.component";
 import {NgModule} from "@angular/core";
 import { CreateComponent } from './crud/create/create.component';
 import { ReadComponent } from './crud/read/read.component';
+import { AboutThatComponent } from './about-that/about-that.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'creator', component: CreateComponent},
   { path: 'searcher', component: ReadComponent},
+  { path: 'about-that', component: AboutThatComponent},
   { path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
